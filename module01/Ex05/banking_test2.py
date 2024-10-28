f03cfc33a04e4b53c596fa000f67e6c53aea531e
M.Kolchuk <m.kolchuk@atronik.ru>
2024-10-28 14:16:45 +0300

    Last one

--- module01/Ex05/banking_test2.py
+++ module01/Ex05/banking_test2.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
         'Smith Jane',
         zip='911-745',
         value=1000.0,
-        ref='1044618427ff2782f0bbece0abd05f31'
+        ref='1044618427ff2782f0bbece0abd05f31',
+        info=None
     ))
     bank.add(Account(
         'William John',
@@ -15,13 +16,15 @@ if __name__ == "__main__":
         ref='58ba2b9954cd278eda8a84147ca73c87',
         info=None
     ))
-
+    print(bank.accounts[0].__dict__)
+    print(bank.accounts[1].__dict__)
     if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
         print('Failed')
 
         bank.fix_account('William John')
         bank.fix_account('Smith Jane')
-
+        print(bank.accounts[0].__dict__)
+        print(bank.accounts[1].__dict__)
     if bank.transfer('William John', 'Smith Jane', 1000.0) is False:
         print('Failed')
     else:
--- module01/Ex05/the_bank.py
+++ module01/Ex05/the_bank.py
@@ -25,6 +25,10 @@ class Account(object):
     def transfer(self, amount):
         self.value += amount
 
+    #def __str__(self):
+    #    return f'{self.name} {self.value}'
+
+
 
 class Bank(object):
 
@@ -32,61 +36,112 @@ class Bank(object):
         self.accounts = []
 
     def add(self, new_account):
-        flag = 0
-        flagA = 1
-        flagB = 0
+        print("added")
         if isinstance(new_account, Account):
-            if(len(new_account.__dict__) % 2 != 0):
-                print("haha")
-                for item in new_account.__dict__:
-                    if(item[0]) == 'b':
-                        flag = 1
+            self.accounts.append(new_account)
 
-                if flag == 0:
-                    for item in new_account.__dict__:
-                        if item == 'zip' or item == 'addr':
-                            flagA = 0
-
-
-                if flagA == 0:
-                    for item in new_account.__dict__:
-                        if item == 'name':
-                            if isinstance(item, str):
-                                flagB += 1
-                            else:
-                                print("name is not a str")
-                            flagB += 1
-
-                        elif item == 'id':
-                            if isinstance(new_account.__dict__[item], int):
-                                flagB += 1
-                            flagB += 1
-
-                        elif item == 'value':
-                            if isinstance(new_account.__dict__[item], float):
-                                flagB += 1
-                            flagB += 1
-                print(flagB)
-                if flagB == 6:
-                    self.accounts.append(new_account)
 
     def transfer(self, origin, dest, amount):
 
-        """" Perform the fund transfer
-        @origin: str(name) of the first account
-        @dest: str(name) of the destination account
-        @amount: float(amount) amount to transfer
-        @return True if success, False if an error occured
-        """
         a = None
         b = None
-
+        flagForTransfer = 0
         for item in self.accounts:
-            print(item)
+            if item.name == dest:
+                b = item
+            elif item.name == origin:
+                a = item
+
+        flag = 0
+        flagA = 1
+        flagB = 0
+
+        if (len(a.__dict__) % 2 != 0):
+            print("haha")
+            for item in a.__dict__:
+                if (item[0]) == 'b':
+                    flag = 1
+
+            if flag == 0:
+                for item in a.__dict__:
+                    if item == 'zip' or item == 'addr':
+                        flagA = 0
+
+            if flagA == 0:
+                for item in a.__dict__:
+                    if item == 'name':
+                        if isinstance(item, str):
+                            flagB += 1
+                        else:
+                            print("name is not a str")
+                        flagB += 1
+
+                    elif item == 'id':
+                        if isinstance(a.__dict__[item], int):
+                            flagB += 1
+                        flagB += 1
+
+                    elif item == 'value':
+                        if isinstance(a.__dict__[item], float):
+                            flagB += 1
+                        flagB += 1
+            print(flagB)
+            if flagB == 6:
+                flagForTransfer += 1
+
+        flag = 0
+        flagA = 1
+        flagB = 0
+
+        if (len(b.__dict__) % 2 != 0):
+            print("haha")
+            for item in b.__dict__:
+                if (item[0]) == 'b':
+                    flag = 1
+
+            if flag == 0:
+                for item in b.__dict__:
+                    if item == 'zip' or item == 'addr':
+                        flagA = 0
+
+            if flagA == 0:
+                for item in b.__dict__:
+                    if item == 'name':
+                        if isinstance(item, str):
+                            flagB += 1
+                        else:
+                            print("name is not a str")
+                        flagB += 1
+
+                    elif item == 'id':
+                        if isinstance(b.__dict__[item], int):
+                            flagB += 1
+                        flagB += 1
+
+                    elif item == 'value':
+                        if isinstance(b.__dict__[item], float):
+                            flagB += 1
+                        flagB += 1
+            print(flagB)
+            if flagB == 6:
+                flagForTransfer += 1
+
+        if flagForTransfer == 2:
+            if amount <= a.value:
+                a.value = a.value - amount
+                b.value = b.value + amount
+        else:
+            return False
+
     # ... Your code ...
     def fix_account(self, name):
         """ fix account associated to name if corrupted
         @name: str(name) of the account
         @return True if success, False if an error occured
         """
-    # ... Your code ...
+        for item in self.accounts:
+            pass
+            if item.name == name:
+                if not hasattr(item, 'addr'):
+                    item.addr = "Somewhere"
+
