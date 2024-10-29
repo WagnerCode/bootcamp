6aa710266f9bd60ba9ddd2c445e4476b7eaafaa6
M.Kolchuk <m.kolchuk@atronik.ru>
2024-10-29 17:06:59 +0300

    f

--- module02/Ex00/ft_filter.py
+++ module02/Ex00/ft_filter.py
@@ -13,9 +13,16 @@ def ft_filter(function_to_apply, iterable):
             yield item
 
 def ft_reduce(function_to_apply, iterable):
+    lol = ""
+    gog = ""
+    for i in range(0, len(iterable) - 1, 2):
+         gog += function_to_apply(iterable[i], iterable[i+1])
+    if len(gog) != len(iterable):
+        gog += iterable[len(iterable) - 1]
+    yield gog
 
-    for i in range(len(iterable)):
-        pass
+
+
 
 
 
@@ -25,8 +32,10 @@ x = [1, 2, 3, 4, 5]
 
 print(list(ft_filter(lambda dum: not (dum % 2), x)))
 
-lst = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+lst = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!']
 
 print(list(ft_reduce(lambda u, v: u + v, lst)))
 
+dd = lambda u, v: u + v
 
+print(dd("5", "5"))
