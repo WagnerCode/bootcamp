b30f6622fb6d97df72445b3616f82c564e78cdf3
M.Kolchuk <m.kolchuk@atronik.ru>
2024-11-14 17:03:56 +0300

    lololo

--- bot.py
+++ bot.py
@@ -3,23 +3,44 @@ import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
+from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="7616332665:AAHSJpkthFBzE7CRiol_CyZ3m5W8WEdTrlM")
+bot = Bot(token=config.bot_token.get_secret_value(),
+          default=DefaultBotProperties(
+              parse_mode=ParseMode.HTML
+          ))
 # Диспетчер
 dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("<u>Hello David!</u>")
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji=DiceEmoji.DICE)
+
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, mylist=[1,2,3])
 
 if __name__ == "__main__":
     asyncio.run(main())
